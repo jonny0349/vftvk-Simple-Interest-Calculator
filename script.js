3f92605eba25f24bedd8c244f21ec62683a34a4e
Jonatan <jonny0349@gmail.com>
2022-03-26 17:40:43 +0000

    Changes made from the theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;      
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);      
+    document.getElementById("result").innerHTML="If you deposit <mark> " +principal+"</mark>,\<br\> at interest rate of <mark>" +rate+ "</mark> %\<br\>You will receive an amount of <mark>" +interest+ "</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+
+     //Validation of a input with amount of money (no negative numbers) 
+     if(principal <= 0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    //If everything is good, the system returns true
+    return true; 
 }
-        
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}  
